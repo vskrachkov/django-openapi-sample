b73ff59aabe8ea4099da28a612d0423ba0c03273
Vyacheslav Krachkov <v.krachkov@enestech.net>
2020-10-25 17:48:59 +0200

    new: add `is_active` field to `User` model

--- users_registry/users/models.py
+++ users_registry/users/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.base_user import AbstractBaseUser
 
 class User(AbstractBaseUser):
     email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
